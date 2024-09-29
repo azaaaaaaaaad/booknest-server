ce14b3ac0dec9c16989b1f660d022553e10b588c
Md Fahim Hossain <fahimreshad3@gmail.com>
2024-09-30 02:00:33 +0600

    solved to import error in api/books page

--- models/Book.js
+++ models/Book.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const BookSchema = new mongoose.Schema({
+ const BookSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
   author: { type: String, required: true },
